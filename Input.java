862a0f0110593cbf0c3a5aac5de1cbaa817ee93f
GPRINAWE <49702490+GPRINAWE@users.noreply.github.com>
2019-10-09 08:27:10 -0700

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,7 +1,29 @@
 import java.util.Scanner;
 
 public class Input {
+
   public static void waitForEnter(Scanner scanner) {
     scanner.nextLine();
+  }
+
+  private static String getUserInput(Scanner scanner, String prompt, String regex) {
+    String input;
+    do {
+        System.out.print(prompt);
+        input = scanner.nextLine().replaceAll(regex, "");
+    } while (input.isBlank());
+    return input;
+  }
+
+  public static String getNormal(Scanner scanner, String prompt)   {
+    return Input.getUserInput(scanner, prompt, "[^A-Za-z0-9]");
+  }
+
+  public static String getLetters(Scanner scanner, String prompt) {
+    return Input.getUserInput(scanner, prompt, "[^A-Za-z]");
+  }
+
+  public static int getDigits(Scanner scanner, String prompt) {
+    return Integer.parseInt(Input.getUserInput(scanner, prompt, "[^0-9]"));
   }
 }
