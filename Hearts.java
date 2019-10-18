635d3c57db67d5220a3aa926bd898b00801652e0
GPRINAWE <49702490+GPRINAWE@users.noreply.github.com>
2019-10-18 08:25:32 -0700

    Update Hearts.java

--- Hearts.java
+++ Hearts.java
@@ -1,16 +1,38 @@
 import java.util.Scanner;
 
 public class Hearts {
+  private static CommandLine actionCmdLn = new CommandLine(
+    "Action (): ",
+    new String[] {""}
+  );
+
   private Scanner scanner;
 
+  private Deck deck;
   private Hand playerHand;
   private Hand[] computerHands = new Hand[3];
 
-  public Hearts(scanner s) {
+  public Hearts(Scanner s) {
     scanner = s;
+
+    deck = new Deck();
+    deck.shuffle();
+
     playerHand = new Hand("hearts");
-    for (int i = 0; i < computerHands.length(); i++) {
+    for (int i = 0; i < computerHands.length; i++) {
       computerHands[i] = new Hand("hearts");
     }
+
+    for (int i = 0; i < 13; i++) {
+      playerHand.addCard(deck.draw());
+      for (Hand compHand : computerHands) {
+        compHand.addCard(deck.draw());
+      }
+    }
+  }
+
+  public double play(double bet) {
+    System.out.println("Your hand: " + playerHand.toString());
+    return 0.0;
   }
 }
