05235c18feebb2941f96b300fb163fa482b32fba
GPRINAWE <49702490+GPRINAWE@users.noreply.github.com>
2019-10-22 08:45:35 -0700

    Update Hearts.java

--- Hearts.java
+++ Hearts.java
@@ -1,40 +1,114 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Hearts {
-  private static CommandLine actionCmdLn = new CommandLine(
-    "Action (): ",
-    new String[] {""}
-  );
+public class Hand {
+  private ArrayList<Card> cards;
+  private String gameType;
 
-  private Scanner scanner;
+  public Hand(String type) {
+    cards = new ArrayList();
+    gameType = type.toLowerCase();
+  }
 
-  private Deck deck;
-  private Hand playerHand;
-  private Hand[] computerHands = new Hand[3];
+  public Hand(String type, ArrayList c) {
+    cards = new ArrayList(c);
+    gameType = type.toLowerCase();
+  }
 
-  public Hearts(Scanner s) {
-    scanner = s;
+  public void addCard(Card cardToAdd) {
+    cards.add(cardToAdd);
+  }
 
-    deck = new Deck();
-    deck.shuffle();
+  public void addCards(ArrayList cardsToAdd) {
+    cards.addAll(cardsToAdd);
+  }
 
-    playerHand = new Hand("hearts");
-    for (int i = 0; i < computerHands.length; i++) {
-      computerHands[i] = new Hand("hearts");
-    }
+  public void sort() {
+    Collections.sort(cards);
+  }
 
-    for (int i = 0; i < 13; i++) {
-      playerHand.addCard(deck.draw());
-      for (Hand compHand : computerHands) {
-        compHand.addCard(deck.draw());
+  public ArrayList<Card> getCards() {
+    return cards;
+  }
+
+  public Card getCard(int index) {
+    return cards.get(index);
+  }
+
+  public Card removeCard(int index) {
+    Card card = cards.get(index);
+    cards.remove(index);
+    return card;
+  }
+
+  public int findCard(int suit, int value) {
+    for (int i = 0; i < cards.size(); i++) {
+      Card card = cards.get(i);
+      if (card.getSuit() == suit && card.getValue() == value) {
+        return i;
       }
+    }
+    return -1;
+  }
+
+  public Card playCard(int suit, int value) {
+    int index = findCard(suit, value);
+    return removeCard(index);
+  }
+
+  public ArrayList<Card> getCardsFromSuit(int suit) {
+    ArrayList result = new ArrayList();
+    for (Card card : cards) {
+      if (card.getSuit() == suit) {
+        result.add(card);
+      }
+    }
+    return result;
+  }
+
+  public ArrayList<Card> getCardsFromValue(int value) {
+    ArrayList result = new ArrayList();
+    for (Card card : cards) {
+      if (card.getValue() == value) {
+        result.add(card);
+      }
+    }
+    return result;
+  }
+
+  public int getHandValue() {
+    switch (gameType) {
+      case "blackjack":
+        int[] pipValMap = new int[] {11,2,3,4,5,6,7,8,9,10,10,10,10};
+        
+        int totalVal = 0;
+        int aceCount = 0;
+        //Caclulate hand value without aces
+        for (Object obj : cards) {
+          Card card = (Card)obj;
+          if (card.getValue() == 1) {
+            aceCount++;
+          } else {
+            totalVal += pipValMap[card.getValue()-1];
+          }
+        }
+        //Add ace values depending of hand value
+        int acesLeft = aceCount;
+        for (int i = 0; i < aceCount; i++) {
+          if (totalVal + acesLeft * pipValMap[0] <= 21) {
+            totalVal += pipValMap[0];
+          } else {
+            totalVal += 1;
+          }
+          acesLeft--;
+        }
+        return totalVal;
+      default: //gameType not recognized
+        return -1;
     }
   }
 
-  public double play(double bet) {
-    System.out.println("Your hand: " + playerHand.toString());
-    playerHand.sort();
-    System.out.println("Sorted: " + playerHand.toString());
-    return 0.0;
+  public String toString() {
+    return cards.toString();
   }
 }
