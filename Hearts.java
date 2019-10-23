e5895437d90c43a0ea618137d3359b8869a586bb
GPRINAWE <49702490+GPRINAWE@users.noreply.github.com>
2019-10-23 09:14:45 -0700

    Update Hearts.java

--- Hearts.java
+++ Hearts.java
@@ -14,6 +14,7 @@ public class Hearts {
   private Scanner scanner;
 
   private Deck deck;
+  private Hand trick;
   private Hand playerHand;
   private Hand[] computerHands = new Hand[3];
 
@@ -45,8 +46,47 @@ public class Hearts {
     return playerHand.playCard(suit, value);
   }
 
+  private Card computerPlayCard(Hand hand) {
+    if (trick.getCards().size() == 0) {
+      int handMin = hand.getMinValue(2);
+    } else {
+      int trickSuit = trick.getCards().get(0).getSuit();
+      Hand trickWithSuit = new Hand("hearts",trick.getCardsFromSuit(trickSuit));
+      Hand handWithSuit = new Hand("hearts",hand.getCardsFromSuit(trickSuit));
+
+      if (handWithSuit.getCards().size() > 0) { //If hand has cards in trick's suit...
+        if (trickWithSuit.getCardsFromValue(1).size() > 0) {
+          //if there is an ace, play highest value of hand
+          return hand.playCard(trickSuit, handWithSuit.getMaxValue(13));
+        } else {
+          //if no ace, try to play highest value you can lose on
+          int trickMax = trickWithSuit.getMaxValue(13);
+          Card card = hand.playCard(trickSuit,handWithSuit.getMaxValue(trickMax-1));
+          if (card == null) {
+            //otherwise accept fate and get rid of highest value
+            card = hand.playCard(trickSuit,handWithSuit.getMaxValue(13));
+          }
+          return card;
+        }
+      } else { //If hand doesn't have cards in trick's suit
+        if (hand.findCard(2,12) != -1) {
+          //play queen of spades at first opportunity
+          return hand.playCard(2,12);
+        } else if (hand.getCardsFromSuit(3).size() > 0) {
+          //play highest heart by default
+          Hand handWithHearts = new Hand("hearts",hand.getCardsFromSuit(3));
+          return hand.playCard(3,handWithHearts.getMaxValue(13));
+        } else {
+          //othewise play highest general card
+          Hand handWithMax = new Hand("hearts",getCardsFromValue(hand.getMaxValue(13)));
+          return hand.playCard(handWithMax.getCard(0).getSuit(),hand.getMaxValue(13));
+        }
+      }
+    }
+  }
+
   public double play(double bet) {
-    Hand trick = new Hand("hearts");
+    trick = new Hand("hearts");
 
     playerHand.sort();
     System.out.println("Your hand: " + playerHand.toString());
@@ -60,15 +100,16 @@ public class Hearts {
 
       if (card == null) {
         System.out.println("Card not found.");
-      } else if (card.getSuit() == 3 && trick.getCards().size() == 0 && !heartsCanLead) {
+      } else if (!heartsCanLead && card.getSuit() == 3 && trick.getCards().size() == 0) {
         System.out.println("Hearts cannot lead until a point card is discarded.");
       } else {
         validPlay = true;
       }
     } while(!validPlay);
 
-    trick.addCard(card);
+    System.out.println();
 
+    trick.addCard(card);
     System.out.println("Trick: " + trick.toString());
     System.out.println("Your hand: " + playerHand.toString());
     return 0.0;
